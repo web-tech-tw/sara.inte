34d7536cc18cf460a30a482e7e3b6bd57554aa92
supersonictw <supersonic@livemail.tw>
2022-04-03 08:45:19 +0000

    Deploying to gh-pages from @ web-tech-tw/sara.inte@0bc1314fd7679b3c020ecfdc083b6ada15f2620d 🚀

---
+++ js/about-legacy.ca347c5e.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksara_inte"]=self["webpackChunksara_inte"]||[]).push([[443],{5830:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var a=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},s=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],u=n(1001),r={},l=(0,u.Z)(r,a,s,!1,null,null,null),i=l.exports}}]);
+//# sourceMappingURL=about-legacy.ca347c5e.js.map
