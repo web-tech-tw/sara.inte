228bc1674cf488c53a249dc6cf6561b186ffaef5
SuperSonic <supersonic@livemail.tw>
2022-04-06 14:10:04 +0800

    Update

--- src/views/ManageView.vue
+++ src/views/ManageView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="flex justify-center my-8 py-16">
-    <div class="flex flex-col">
+    <div class="flex flex-col mx-5 md:mx-auto">
       <div class="max-w-2xl mx-auto sm:px-6 lg:px-8">
         <div v-if="!edit" class="overflow-hidden shadow-md">
           <div class="px-6 py-4 bg-white border-b border-gray-200 font-bold">
@@ -8,8 +8,8 @@
           </div>
           <div class="p-6 bg-white border-b border-gray-200">
             <span class="text-gray-600">暱稱：</span>{{ profile.nickname }}<br/>
-            <span class="text-gray-600">Sara系統使用者識別碼：</span>{{ profile._id }}<br/>
-            <span class="text-gray-600">電子郵件地址：</span>{{ profile.email }}
+            <span class="text-gray-600">電子郵件地址：</span>{{ profile.email }}<br/>
+            <span class="text-gray-600">Sara系統使用者識別碼：</span>{{ profile._id }}
           </div>
           <div class="p-6 bg-white border-gray-200 text-right">
             <button
@@ -31,7 +31,7 @@
               <label class="px-4 py-1 text-gray-600" for="nickname">暱稱：</label>
               <input
                   id="nickname"
-                  :value="profile.nickname"
+                  v-model="field.nickname"
                   class="border-none bg-transparent px-4 py-1 text-gray-900 outline-none focus:outline-none"
                   placeholder="暱稱"
                   type="text"
@@ -50,7 +50,7 @@
             </button>
             <button
                 class="bg-sky-500 shadow-md text-sm text-white font-bold py-3 md:px-8 px-4 hover:bg-sky-600 rounded"
-                @click="edit = false">確定修改
+                @click="update">確定修改
             </button>
           </div>
         </div>
@@ -65,7 +65,10 @@ import jwtDecode from 'jwt-decode';
 export default {
   name: 'ManageView',
   data: () => ({
-    edit: false
+    edit: false,
+    field: {
+      nickname: ''
+    }
   }),
   computed: {
     profile() {
@@ -76,12 +79,35 @@ export default {
       const profile = jwtDecode(token);
       return profile?.user || null;
     },
+    authOptions() {
+      return {headers: {Authorization: localStorage.getItem('unified_token')}}
+    }
   },
   methods: {
+    update() {
+      const form = new URLSearchParams();
+      form.set('nickname', this.field.nickname);
+      this.$axios.put('/profile', form, this.authOptions)
+          .then((xhr) => {
+            if (xhr?.data?.token) {
+              this.status = '修改成功，正在更新憑證...';
+              localStorage.setItem('unified_token', xhr.data.token);
+              setTimeout(() => location.reload(), 500);
+            } else {
+              this.status = '發生錯誤 (無錯誤代碼)';
+            }
+          })
+          .catch((error) => {
+            this.status = `發生錯誤 (${error?.response?.status || '無錯誤代碼'})`;
+          });
+    },
     logout() {
       localStorage.clear();
       location.assign('https://web-tech-tw.github.io')
     }
+  },
+  mounted() {
+    this.field.nickname = this.profile.nickname;
   }
 }
 </script>
