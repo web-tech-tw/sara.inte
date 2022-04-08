096392475da89f96b5d4f9e33d93801e7343a48e
SuperSonic <supersonic@livemail.tw>
2022-04-08 18:11:11 +0800

    Update

--- src/router/index.js
+++ src/router/index.js
@@ -36,20 +36,21 @@ const router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   if (localStorage.getItem('unified_token')) {
-    if (location.query) {
-      const params = new URLSearchParams(location.query)
+    if (location.search) {
+      const params = new URLSearchParams(location.search);
       if (params.has('refer')) {
-        replaceToLocationSafe(params.get('refer'))
+        replaceToLocationSafe(params.get('refer'));
+        return;
       }
     }
     if (to.name !== 'manage' && to.name !== 'manage-email') {
       next({name: 'manage'});
     }
   } else {
-    if (location.query) {
-      const params = new URLSearchParams(location.query)
+    if (location.search) {
+      const params = new URLSearchParams(location.search);
       if (params.has('refer')) {
-        sessionStorage.setItem('sara_refer', params.get('refer'))
+        sessionStorage.setItem('sara_refer', params.get('refer'));
       }
     }
     if (to.name === 'manage') {
