1907cde2aa5d1e77cf48d17c2d1dbe82fe86fd59
SuperSonic <supersonic@livemail.tw>
2023-09-25 05:59:15 +0000

    fix: menuItems

--- src/components/AppHeaderMenuData.js
+++ src/components/AppHeaderMenuData.js
@@ -1,3 +1,7 @@
+const {
+  VITE_SARA_INTE_HOST: websiteUrl,
+} = import.meta.env;
+
 export const titleLong = 'Sara - 無密碼式身份認證系統';
 export const titleShort = 'Sara';
 
@@ -8,6 +12,6 @@ export const menuItems = [
     name: "回首頁",
     type: "function",
     icon: 'HomeIcon',
-    action: () => location.assign(process.env.VUE_APP_WEBSITE_URL)
+    onClick: () => location.assign(websiteUrl)
   },
 ]
